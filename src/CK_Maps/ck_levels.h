e741e7e139b467b5f8d6e512bd5af932452117cc
John314 <36487623+john314thegeekygenius@users.noreply.github.com>
2022-04-14 14:05:57 -0400

    Add files via upload

--- src/CK_Maps/ck_levels.h
+++ src/CK_Maps/ck_levels.h
@@ -1,18 +1,20 @@
 
 
 #include "ck5_tileset0_info.h"
-#ifdef LK_4BPP_MAPS
-#include "ck5_tileset04b.h"
-#include "ck5_tileset14b.h"
-#else
+#include "ck5_tileset1_info.h"
+//#ifdef LK_4BPP_MAPS
+//#include "ck5_tileset04b.h"
+//#include "ck5_tileset14b.h"
+//#else
 #include "ck5_tileset0.h"
 #include "ck5_tileset1.h"
-#endif
+//#endif
 
 #include "ck_map0.h"
 #include "ck_map1.h"
 #include "ck_map2.h"
 #include "ck_map3.h"
+#include "ck_map4.h"
 
 
 // Define music ids
@@ -20,6 +22,7 @@ const uint16_t ck_map0_music = 0;
 const uint16_t ck_map1_music = 1;
 const uint16_t ck_map2_music = 2;
 const uint16_t ck_map3_music = 3;
+const uint16_t ck_map4_music = 1;
 
 
 // Define map info
@@ -47,11 +50,18 @@ const char *ck_map3_info[] = {
 	"60 x 60"
 };
 
+const char *ck_map4_info[] = {
+	"Bridge Drop",
+	"By Ceilick",
+	"52 x 27"
+};
+
 const uint16_t *ck_levels_data[] = {
 	&ck_map0_data,
 	&ck_map1_data,
 	&ck_map2_data,
 	&ck_map3_data,
+	&ck_map4_data,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
@@ -65,6 +75,7 @@ const unsigned char ** ck_levels_tileset[] = {
 	&ck_map1_tileset,
 	&ck_map2_tileset,
 	&ck_map3_tileset,
+	&ck_map4_tileset,
 	0,
 	0,
 	0,
@@ -80,6 +91,7 @@ const uint16_t *ck_levels_tileinfo[] = {
 	&ck5_tileset0_tileinfo,
 	&ck5_tileset0_tileinfo,
 	&ck5_tileset0_tileinfo,
+	&ck5_tileset1_tileinfo,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
@@ -93,6 +105,7 @@ const uint16_t ck_levels_width[] = {
 	ck_map1_width,
 	ck_map2_width,
 	ck_map3_width,
+	ck_map4_width,
 	0,
 	0,
 	0,
@@ -107,6 +120,7 @@ const uint16_t ck_levels_height[] = {
 	ck_map1_height,
 	ck_map2_height,
 	ck_map3_height,
+	ck_map4_height,
 	0,
 	0,
 	0,
@@ -121,6 +135,7 @@ const char ** ck_levels_info[] = {
 	&ck_map1_info,
 	&ck_map2_info,
 	&ck_map3_info,
+	&ck_map4_info,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
@@ -134,6 +149,7 @@ const uint16_t ck_levels_music[] = {
 	ck_map1_music,
 	ck_map2_music,
 	ck_map3_music,
+	ck_map4_music,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
 	(uint16_t*)NULL,
